3d2407151520967dc457b7cba4e4eddbb55e4d8b
AlexaVane <alexavanemacas321@gmail.com>
2018-11-13 23:53:30 -0500

    deber

--- 02-Android/Palet/app/src/main/java/com/example/ale/palet/MainActivity.kt
+++ 02-Android/Palet/app/src/main/java/com/example/ale/palet/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.ale.palet
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Ingresar_login_Btn.setOnClickListener{
+
+            var nom =nombre_editxt.getText().toString()
+        var pass=pass_editxt.getText().toString()
+            if (nom.equals("Ale")&&pass.equals("123") ) {
+                irApantallados()
+            }
+            else
+            {
+                Toast.makeText(this, "Usuario Incorrecto",Toast.LENGTH_SHORT)
+            }
+        }
+
+
     }
+    fun irApantallados(){
+   var intentIrPantalla = Intent(this,pantallados::class.java)
+        startActivity(intentIrPantalla)
+    }
+
 }
---
+++ 02-Android/Palet/app/src/main/java/com/example/ale/palet/pantallados.kt
@@ -0,0 +1,12 @@
+package com.example.ale.palet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class pantallados : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantallados)
+    }
+}
